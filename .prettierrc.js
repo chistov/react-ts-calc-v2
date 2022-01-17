b2c005d56533db90ae1ab0c939762b2b9734e011
Sergey Chistov <s.chistov@globus-ltd.com>
2022-01-17 23:14:05 +0300

    test husky

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  importOrderSeparation: false,
+};
